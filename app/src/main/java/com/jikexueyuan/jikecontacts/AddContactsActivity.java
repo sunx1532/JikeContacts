bfd15bc0683c262db9803b5ac688598caa0d04af
RogerSun <hehedehehe@gmail.com>
2016-04-17 00:16:05 +0800

    MacPro提交 代码基本完成， UI需要调整

--- app/src/main/java/com/jikexueyuan/jikecontacts/AddContactsActivity.java
+++ app/src/main/java/com/jikexueyuan/jikecontacts/AddContactsActivity.java
@@ -1,24 +1,27 @@
 package com.jikexueyuan.jikecontacts;
 
 import android.app.Activity;
+import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
-
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.provider.ContactsContract.CommonDataKinds.StructuredName;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
-import android.provider.ContactsContract.Contacts.Data;
+import android.provider.ContactsContract.CommonDataKinds.StructuredName;
+import android.provider.ContactsContract.Data;
+import android.provider.ContactsContract.RawContacts;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 
 public class AddContactsActivity extends Activity implements View.OnClickListener {
 
     Button btnAddOk = null;
     Button btnAddCancel = null;
+
+    ContentProvider contentProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,32 +34,48 @@ public class AddContactsActivity extends Activity implements View.OnClickListene
         btnAddOk.setOnClickListener(this);
         btnAddCancel.setOnClickListener(this);
 
+
+
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.btnAddOk:
-                /*etName = ((EditText) findViewById(R.id.etName)).getText().toString();
-                etNumber = ((EditText) findViewById(R.id.etNumber)).getText().toString();
-                System.out.println("Contacts:"+ etName + etNumber);*/
-                EditText etName = (EditText) findViewById(R.id.etName);
-                EditText etNumber = (EditText) findViewById(R.id.etNumber);
 
-                Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                String etName = ((EditText) findViewById(R.id.etName)).getText().toString();
+                String etNumber = ((EditText) findViewById(R.id.etNumber)).getText().toString();
 
-                intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+                ContentValues values = new ContentValues();
+                Uri rawContactUri = getContentResolver().insert(RawContacts.CONTENT_URI, values);
+                long rawContactId = ContentUris.parseId(rawContactUri);
 
-                intent.putExtra(ContactsContract.Intents.Insert.NAME, etName.getText().toString());
+                values.clear();
+                values.put(RawContacts.Data.RAW_CONTACT_ID, rawContactId);
+                // 设置内容类型
+                values.put(RawContacts.Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE);
+                // 设置联系人名字
+                values.put(StructuredName.GIVEN_NAME, etName);
+                // 向联系人URI添加联系人名字
+                getContentResolver().insert(Data.CONTENT_URI, values);
 
-                intent.putExtra(ContactsContract.Intents.Insert.PHONE, etNumber.getText());
+                values.clear();
+                values.put(RawContacts.Data.RAW_CONTACT_ID, rawContactId);
+                values.put(RawContacts.Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE);
+                // 设置联系人的电话号码
+                values.put(Phone.NUMBER, etNumber);
+                // 设置电话类型
+                values.put(Phone.TYPE, Phone.TYPE_MOBILE);
+                // 向联系人电话号码URI添加电话号码
+                getContentResolver().insert(Data.CONTENT_URI, values);
 
-                startActivity(intent);
-
+                AddContactsActivity.this.setResult(RESULT_OK);
+                AddContactsActivity.this.finish();
                 break;
 
             case R.id.btnAddCancel:
-
+                AddContactsActivity.this.setResult(RESULT_CANCELED);
+                AddContactsActivity.this.finish();
                 break;
         }
     }
--- app/src/main/java/com/jikexueyuan/jikecontacts/MainActivity.java
+++ app/src/main/java/com/jikexueyuan/jikecontacts/MainActivity.java
@@ -1,14 +1,22 @@
 package com.jikexueyuan.jikecontacts;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,18 +34,62 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        makeList();
+
         Button btnAdd = (Button) findViewById(R.id.btnAdd);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddContactsActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
 
-                 Intent intent = new Intent(MainActivity.this, AddContactsActivity.class);
+        contactsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final String contacts[] = contactsList.get(position).split("\n");
 
-                 startActivityForResult(intent,0);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.menuTitle);
 
-             }
-         });
+                String strName = getString(R.string.call);
+                String strNumber = getString(R.string.sms);
+                final String[] options = {strName, strNumber};
+
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + contacts[1]));
+                                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                                    // TODO: Consider calling
+                                    //    ActivityCompat#requestPermissions
+                                    // here to request the missing permissions, and then overriding
+                                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                    //                                          int[] grantResults)
+                                    // to handle the case where the user grants the permission. See the documentation
+                                    // for ActivityCompat#requestPermissions for more details.
+                                    return;
+                                }
+                                startActivity(callIntent);
+                                break;
+                            case 1:
+                                Intent smsIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + contacts[1]));
+                                startActivity(smsIntent);
+                                break;
+                        }
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+    private void makeList(){
+        contactsList.clear();
 
         contactsView = (ListView) findViewById(R.id.listViewContacts);
 
@@ -46,18 +98,15 @@ public class MainActivity extends Activity {
         contactsView.setAdapter(adapter);
 
         readContacts();
-
-
     }
-
 
     private void readContacts(){
         Cursor cursor = null;
         try{
-            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null);
+            cursor = getContentResolver().query(Phone.CONTENT_URI,null,null,null,null);
             while (cursor.moveToNext()){
-                String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-                String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                String displayName = cursor.getString(cursor.getColumnIndex(Phone.DISPLAY_NAME));
+                String number = cursor.getString(cursor.getColumnIndex(Phone.NUMBER));
                 contactsList.add(displayName + "\n" + number);
             }
         }catch (Exception e){
@@ -68,4 +117,21 @@ public class MainActivity extends Activity {
             }
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (resultCode){
+            case RESULT_OK:
+                Toast.makeText(MainActivity.this,R.string.Saved,Toast.LENGTH_SHORT).show();
+
+                makeList();
+                break;
+            case RESULT_CANCELED:
+                Toast.makeText(MainActivity.this,R.string.Cancel,Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+
 }
